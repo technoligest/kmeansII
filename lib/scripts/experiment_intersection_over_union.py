2645de244cc0bd1a51ce20942046b0060bb1b8f4
Yaser <technoligest@gmail.com>
2017-08-02 22:38:13 -0300

    added a simple change

--- lib/scripts/experiment_intersection_over_union.py
+++ lib/scripts/experiment_intersection_over_union.py
@@ -2,12 +2,11 @@ from distance import findBelongingCentrePosition, findDistanceMatrix
 from hungarian import minimum_weight_perfect_matching
 class calcIntersections:
   def rearrangeClusterCentres(self, baseExperiment, otherExperiments):
-    matchings = minimum_weight_perfect_matching(findDistanceMatrix(baseExperiment.centres, otherExperiments.centres))
-    baseExperiment.centres=[baseExperiment[x] for x,_ in matchings]
+    
   def calcNorbertArray(self):
     for i in range(1,len(self.experiments)):
-      rearrangeClusterCentres(experiments[0], experiments[i])
-    return [[findBelongingCentrePosition(self.dataset[instId],self.experiments[experimentId].centres) for intId in range(len(self.dataset))]for experimentId in range(len(self.experiments))]
+      self.experiment[i].centres = [self.experiments[i].centres[x] for x,_ in minimum_weight_perfect_matching(findDistanceMatrix(self.experiments[0].centres, self.experiments[i].centres))]
+    return [[findBelongingCentrePosition(self.dataset[instId],self.experiments[experimentId].centres) for instId in range(len(self.dataset))]for experimentId in range(len(self.experiments))]
   
   def __init__(self, dataset, experiments):
     self.dataset=dataset
